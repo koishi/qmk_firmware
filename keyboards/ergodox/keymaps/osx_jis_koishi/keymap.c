b5c266bc2e8660b91610819635f14bd0e8b045a2
Koichiro Oishi <bs@v7.com>
2017-09-10 16:49:47 +0900

    change number key

--- keyboards/ergodox/keymaps/osx_jis_koishi/keymap.c
+++ keyboards/ergodox/keymaps/osx_jis_koishi/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   1    |   2  |   3  |   4  |   5  |   6  | Hyper|           | PrScr|   7  |   8  |   9  |   0  |   -  |   ^    |
+ * | esc    |   1  |   2  |   3  |   4  |   5  |   6  |           |   7  |   8  |   9  |   0  |   -  |   ^  |   ¥    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Down |           |  Up  |   Y  |   U  |   I  |   O  |   P  |   @    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_ESC,
         // right hand
-             KC_PSCR,     KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,          KC_EQL,
+             KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,           KC_JYEN,
              KC_UP,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              KC_RIGHT,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RO,
