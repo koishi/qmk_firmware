711472e58de69b79032ff2caac995a7ffec000ea
Koichiro Oishi <bs@v7.com>
2017-10-16 10:30:25 +0900

    change BSPC

--- keyboards/ergodox/keymaps/osx_jis_koishi/keymap.c
+++ keyboards/ergodox/keymaps/osx_jis_koishi/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_ESC,
         // right hand
-             KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,           KC_JYEN,
+             KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,           KC_BSPC,
              KC_UP,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              KC_RIGHT,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RO,
