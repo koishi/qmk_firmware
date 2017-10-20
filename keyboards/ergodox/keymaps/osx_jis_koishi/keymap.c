ac5a1e3a69735a1044396dec22746b177fcd0f48
Koichiro Oishi <bs@v7.com>
2017-10-20 12:12:12 +0900

    update new layout

--- keyboards/ergodox/keymaps/osx_jis_koishi/keymap.c
+++ keyboards/ergodox/keymaps/osx_jis_koishi/keymap.c
@@ -33,23 +33,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_DOWN,
-        KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LEFT,
-        KC_FN1,         KC_LALT,      KC_LGUI,KC_TRNS,KC_LANG2,
-                                              ALT_T(KC_APP),  KC_HOME,
+        KC_ESC,         KC_1,         KC_2,        KC_3,     KC_4,   KC_5,   KC_6,
+        KC_TAB,         KC_Q,         KC_W,        KC_E,      KC_R,   KC_T,  KC_WH_D,
+        KC_LCTRL,       KC_A,         KC_S,        KC_D,      KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,  KC_WH_U,
+        KC_TRNS,        ALL_T(KC_NO), KC_TRNS,     KC_LALT,   GUI_T(KC_LANG2),
+                                               KC_FN1,  KC_HOME,
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_ESC,
         // right hand
-             KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,           KC_JYEN,
+             KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,           KC_BSPC,
              KC_UP,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             KC_RIGHT,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RO,
+             KC_RIGHT,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_RO),
                                 KC_LANG1, KC_JYEN,KC_RBRC,KC_BSLS,          KC_FN2,
-             KC_PGUP,        KC_LCTRL,
-             KC_PGDN,
-             KC_LALT,KC_DEL, KC_ENT
+                          GUI_T(KC_LANG1),  KC_JYEN,KC_RBRC,KC_BSLS,          KC_FN2,
+             KC_LEFT,        KC_RIGHT,
+             KC_UP,
+             KC_DOWN,KC_DEL, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
