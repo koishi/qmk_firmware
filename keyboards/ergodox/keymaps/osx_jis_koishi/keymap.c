2e2ac3d870161cf7d1d1a0b2110ee8d2d7888fb8
Koichiro Oishi <bs@v7.com>
2017-10-26 14:23:57 +0900

    変えてみた

--- keyboards/ergodox/keymaps/osx_jis_koishi/keymap.c
+++ keyboards/ergodox/keymaps/osx_jis_koishi/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,        KC_E,      KC_R,   KC_T,  KC_WH_D,
         KC_LCTRL,       KC_A,         KC_S,        KC_D,      KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,  KC_WH_U,
-        KC_TRNS,        ALL_T(KC_NO), KC_TRNS,     KC_LALT,   GUI_T(KC_LANG2),
+        KC_TRNS,        ALL_T(KC_NO), KC_TRNS,     KC_LALT,   KC_LGUI,
                                                KC_FN1,  KC_HOME,
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_ESC,
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_UP,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              KC_RIGHT,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_RO),
-                          GUI_T(KC_LANG1),  KC_JYEN,KC_RBRC,KC_BSLS,          KC_FN2,
+                          KC_RGUI,KC_JYEN,KC_RBRC,KC_BSLS,          KC_FN2,
              KC_LEFT,        KC_RIGHT,
              KC_UP,
              KC_DOWN,KC_DEL, KC_ENT
