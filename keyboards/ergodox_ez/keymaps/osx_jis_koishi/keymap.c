a9e27133ce6e9ac61c47e3e187afc5e91cae3a0c
Koichiro Oishi <bs@v7.com>
2018-01-11 11:23:10 +0900

    change KC_RBRC, KC_BSLS

--- keyboards/ergodox_ez/keymaps/osx_jis_koishi/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx_jis_koishi/keymap.c
@@ -43,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_SPC,KC_BSPC,KC_ESC,
         // right hand
              KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_JYEN,          KC_BSPC,
-             KC_UP,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
+             KC_RBRC,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             KC_RIGHT,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_RO),
+             KC_BSLS,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_RO),
                           KC_RGUI,KC_EQL,KC_RBRC,KC_BSLS,          KC_FN2,
              KC_LEFT,        KC_RIGHT,
              KC_UP,
