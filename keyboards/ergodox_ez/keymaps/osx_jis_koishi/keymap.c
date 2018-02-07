ec9e67b58c8e57cb9745a6871cf96860fc080fbb
Koichiro Oishi <bs@v7.com>
2018-02-07 11:29:16 +0900

    remove unused keymap

--- keyboards/ergodox_ez/keymaps/osx_jis_koishi/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx_jis_koishi/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RBRC,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              KC_BSLS,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_RO),
-                          KC_RGUI,KC_EQL,KC_RBRC,KC_BSLS,          KC_FN2,
+                          KC_RGUI,KC_EQL,KC_TRNS,KC_TRNS,          KC_FN2,
              KC_LEFT,        KC_RIGHT,
              KC_UP,
              KC_DOWN,KC_DEL, KC_ENT
