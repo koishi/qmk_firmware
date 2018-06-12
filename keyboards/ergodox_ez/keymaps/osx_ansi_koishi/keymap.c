80dc4f6e568dee73a9f41a5af2fef4fbb0b0d85a
Koichiro Oishi <bs@v7.com>
2018-06-12 10:20:06 +0900

    Change cursor key assign

--- keyboards/ergodox_ez/keymaps/osx_ansi_koishi/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx_ansi_koishi/keymap.c
@@ -38,18 +38,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTRL,       KC_A,         KC_S,        KC_D,      KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,  KC_WH_U,
         KC_TRNS,        ALL_T(KC_NO), KC_TRNS,     KC_LALT,   KC_LGUI,
-                                                   KC_FN1,    KC_HOME,
+                                                   KC_FN1,    KC_FN2,
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_ESC,
         // right hand
-             KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,           KC_BSPC,
-             KC_LBRC,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             KC_RBRC,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_GRV,
-                          KC_RGUI,KC_TRNS,KC_TRNS,KC_TRNS,          KC_FN2,
-             KC_LEFT,        KC_RIGHT,
-             KC_UP,
-             KC_DOWN,KC_DEL, KC_ENT
+        KC_7,           KC_8,         KC_9,        KC_0,      KC_MINS,KC_EQL,   KC_BSPC,
+        KC_LBRC,        KC_Y,         KC_U,        KC_I,      KC_O,   KC_P,     KC_BSLS,
+                        KC_H,         KC_J,        KC_K,      KC_L,   KC_SCLN,  KC_QUOT,
+        KC_RBRC,        KC_N,         KC_M,        KC_COMM,   KC_DOT, KC_SLSH,  KC_GRV,
+                        KC_RGUI,      KC_LEFT,     KC_UP,     KC_DOWN,KC_RIGHT,
+        KC_TRNS,        KC_TRNS,
+        KC_TRNS,
+        KC_TRNS,KC_DEL, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
