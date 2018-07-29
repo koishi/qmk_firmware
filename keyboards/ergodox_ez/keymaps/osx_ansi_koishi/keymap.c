12ee429852dce693844378878225e29549a32b21
Koichiro Oishi <bs@v7.com>
2018-07-29 17:24:58 +0900

    とりあえずこれで

--- keyboards/ergodox_ez/keymaps/osx_ansi_koishi/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx_ansi_koishi/keymap.c
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,        KC_E,      KC_R,   KC_T,  KC_WH_D,
         KC_LCTRL,       KC_A,         KC_S,        KC_D,      KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,  KC_WH_U,
-        MO(SYMB),       MO(MDIA),     KC_TRNS,     KC_LALT,   KC_LGUI,
-                                                   KC_FN1,    KC_FN2,
+        KC_TRNS,        KC_TRNS,      KC_TRNS,     KC_LALT,   GUI_T(KC_LANG2),
+                                                   MO(SYMB),  MO(MDIA),
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_ESC,
         // right hand
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LBRC,        KC_Y,         KC_U,        KC_I,      KC_O,   KC_P,     KC_BSLS,
                         KC_H,         KC_J,        KC_K,      KC_L,   KC_SCLN,  KC_QUOT,
         KC_RBRC,        KC_N,         KC_M,        KC_COMM,   KC_DOT, KC_SLSH,  KC_GRV,
-                        KC_RGUI,      KC_LEFT,     KC_UP,     KC_DOWN,KC_RIGHT,
+                        GUI_T(KC_LANG1),      KC_LEFT,     KC_UP,     KC_DOWN,KC_RIGHT,
         KC_LANG2,        KC_LANG1,
         KC_TRNS,
         KC_TRNS,KC_DEL, KC_ENT
