f3d026a3904eaeb168150876c8d296b9bc17e23e
Koichiro Oishi <bs@v7.com>
2018-07-29 17:00:06 +0900

    レイヤー切り替えを別で配置

--- keyboards/ergodox_ez/keymaps/osx_ansi_koishi/keymap.c
+++ keyboards/ergodox_ez/keymaps/osx_ansi_koishi/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,        KC_E,      KC_R,   KC_T,  KC_WH_D,
         KC_LCTRL,       KC_A,         KC_S,        KC_D,      KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,        KC_C,      KC_V,   KC_B,  KC_WH_U,
-        KC_TRNS,        ALL_T(KC_NO), KC_TRNS,     KC_LALT,   KC_LGUI,
+        MO(SYMB),       MO(MDIA),     KC_TRNS,     KC_LALT,   KC_LGUI,
                                                    KC_FN1,    KC_FN2,
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_ESC,
