7ac90528a11142d0e890f74e82206f45d67b850d
Koichiro Oishi <bs@v7.com>
2018-09-19 08:31:12 +0900

    Add mousekey

--- keyboards/ergodash/rev2/keymaps/koishi/config.h
+++ keyboards/ergodash/rev2/keymaps/koishi/config.h
@@ -42,3 +42,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
+
+#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_TIME_TO_MAX    60
+#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_WHEEL_DELAY 0
